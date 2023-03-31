a2b48f2860f8c55fea0aa66fb6e6893c103b36c8
kazirumi <tangorumi@gmail.com>
2023-03-31 15:31:55 +0600

    L-3 Polymorfic Relation ships through tinker

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+    public function image(){
+        return $this->morphOne(Image::class,'imageable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class,'commentable');
+    }
+}
